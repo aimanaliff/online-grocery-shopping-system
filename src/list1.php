44dc21d04bc2807a402435e54dfd8783e3b54794
Rush-2K <syaidiazhari@gmail.com>
2021-06-03 14:40:51 +0800

    Update list1.php
    post method problem not solve

--- src/list1.php
+++ src/list1.php
@@ -2,13 +2,14 @@
 
 include("../includes/header.php");
 include("../functions/listmethod.php");
+
 ?>
 
 <div class="container-xl mt-4 mb-4" style="background-color: white;">
 
-<div class="row mb-5" style="background-color: #ED7D31;">
-    <h3 style="text-align: center;" class="py-3 rounded-3">Shopping List</h3>
-</div>
+    <div class="row mb-5" style="background-color: #ED7D31;">
+        <h3 style="text-align: center;" class="py-3 rounded-3">Shopping List</h3>
+    </div>
 
     <?php 
             
@@ -16,45 +17,54 @@ include("../functions/listmethod.php");
         
     ?>
 
-<div class="row">
-    <div class="center m-3">
-        <button type="button" class="btn btn-outline-success" id="buttonmodal" data-bs-toggle="modal" data-bs-target="#AddName" style="width: 30%;">
-            <strong>+</strong> Create List
-          </button>
-    </div>
-    <!-- Modal -->
-    <div class="modal fade" id="AddName" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header text-center">
-                    <h5 class="modal-title" id="staticBackdropLabel">List Name</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body" style="text-align: center;">
-                    <input type="text" placeholder="Add a name" style="text-align: center;">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save</button>
+    <div class="row">
+        <form class="center m-3">
+            <button type="button" class="btn btn-outline-success" id="buttonmodal" data-bs-toggle="modal" data-bs-target="#AddName" style="width: 30%;">
+                <strong>+</strong> Create List
+            </button>
+        </div>
+        <!-- Modal -->
+        <!-- <form method="POST"> -->
+        <div class="modal fade" id="AddName" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                <form method="POST">
+                    <div class="modal-header text-center">
+                        <h5 class="modal-title" id="staticBackdropLabel">List Name</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" style="text-align: center;">
+                        <input type="text" placeholder="Add a name" style="text-align: center;" name="listname">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" name="submit3" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
                 </div>
             </div>
         </div>
-    </div>
-
-    
-
-    <?php 
+        <!-- <form> -->
         
 
-    
-    
-    ?>
+        <?php 
+            
+            if(isset($_POST['submit3'])){
+                // $sql = 'SELECT * FROM listname';
+                // $result = mysqli_query($db, $sql);
+                // $listname = mysqli_fetch_array($result);
+                echo $_POST['listname'];
+            }else{
+                echo 'none';
+            }
+        
+        ?>
+
+    </div>
 
 </div>
 
-</div>
-
-          <?php 
+        <?php 
 
           include("../includes/footer.php")
 
